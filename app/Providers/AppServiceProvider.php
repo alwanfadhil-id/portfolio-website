0e7ac8c6c984ca43838bdc7aa1bd9ef4d43faaba
alwanfadhil-id <alwanfadhil.id@gmail.com>
2025-06-23 14:51:07 +0700

    update v1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,26 +8,19 @@ use App\Models\Setting;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        // Bagikan setting ke semua view
         View::composer('*', function ($view) {
             $settings = [
                 'site_title' => Setting::get('site_title', 'My Portfolio'),
                 'site_description' => Setting::get('site_description', 'Welcome to my portfolio'),
                 'favicon' => Setting::get('favicon'),
-                'logo' => Setting::get('logo'),
+                'profile' => Setting::get('profile'),
                 'about_me' => Setting::get('about_me', 'Tell something about yourself'),
                 'contact_email' => Setting::get('contact_email', 'your@email.com'),
                 'github_url' => Setting::get('github_url'),
