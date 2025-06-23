4dc27f08b48577a9361aa8f825676a15e14c6ba2
alwanfadhil-id <alwanfadhil.id@gmail.com>
2025-06-23 14:10:21 +0700

    Membuat Routes Admin

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@
     use App\Http\Controllers\ProjectController;
     use App\Http\Controllers\AdminAuthController;
     use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\AdminController;
 
 
     /*
@@ -28,3 +29,14 @@
     Route::get('/contact', [ContactController::class, 'index'])->name('contact');
     Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
 
+    Route::prefix('hidden-admin-panel')->group(function () {
+    Route::get('/login', [AdminController::class, 'login'])->name('admin.login');
+    Route::post('/login', [AdminController::class, 'authenticate'])->name('admin.authenticate');
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('/settings', [AdminController::class, 'settings'])->name('admin.settings');
+    Route::post('/settings', [AdminController::class, 'updateSettings'])->name('admin.settings.update');
+    Route::get('/messages', [AdminController::class, 'messages'])->name('admin.messages');
+    Route::delete('/messages/{id}', [AdminController::class, 'deleteMessage'])->name('admin.messages.delete');
+    Route::get('/projects', [AdminController::class, 'projects'])->name('admin.projects');
+    Route::post('/logout', [AdminController::class, 'logout'])->name('admin.logout');
+});
