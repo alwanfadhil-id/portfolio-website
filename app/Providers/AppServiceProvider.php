c1fed3203f3b7c9c560cf9b8053c3739d8cccc15
alwanfadhil-id <alwanfadhil.id@gmail.com>
2025-06-23 14:38:55 +0700

    AppServiceProvider Duplikasi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Setting;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Bagikan setting ke semua view
+        View::composer('*', function ($view) {
+            $settings = [
+                'site_title' => Setting::get('site_title', 'My Portfolio'),
+                'site_description' => Setting::get('site_description', 'Welcome to my portfolio'),
+                'favicon' => Setting::get('favicon'),
+                'logo' => Setting::get('logo'),
+                'about_me' => Setting::get('about_me', 'Tell something about yourself'),
+                'contact_email' => Setting::get('contact_email', 'your@email.com'),
+                'github_url' => Setting::get('github_url'),
+                'linkedin_url' => Setting::get('linkedin_url'),
+            ];
+            
+            $view->with('siteSettings', $settings);
+        });
     }
 }
